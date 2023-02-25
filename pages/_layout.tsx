a41208f028aeae8aec49292af53f813c96514d6c
arthurlch <lachat.arthur74@gmail.com>
2023-02-25 16:26:20 +0900

    fix: menu icon padding issue, changed for noew icons and menu is trimmed for useless functionnality

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -1,3 +1,6 @@
+import { HiOutlineMenu } from 'react-icons/hi';
+
+
 export default function Layout({
     children, 
   }: {
@@ -7,23 +10,24 @@ export default function Layout({
         <>
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">Snippet Hive</a>
                 </div>
                 <div className="flex-none gap-2">
-                    <div className="form-control">
-                    <input type="text" placeholder="Search" className="input input-bordered" />
-                    </div>
                     <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        <div className=" rounded-full content-center p-0">
+                            <HiOutlineMenu className='p-0 w-7 h-7' />                        
                         </div>
                     </label>
                     <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                         <li>
                         <a className="justify-between">
+                            Snippets
+                        </a>
+                        </li>
+                        <li>
+                        <a className="justify-between">
                             Profile
-                            <span className="badge">New</span>
                         </a>
                         </li>
                         <li><a>Settings</a></li>
