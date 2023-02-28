63949b52e301ed7a3eac0b4671bc64db1af72765
arthurlch <lachat.arthur74@gmail.com>
2023-02-28 23:19:38 +0900

    chores: add linter x prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Layout from './_layout'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import Layout from './_layout';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <Layout>
-      <div>
-        Test
-      </div>
+      <div>Test</div>
     </Layout>
-  )
+  );
 }
