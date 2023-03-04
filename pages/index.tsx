32bddfabbce27166d611e4006f07e56e3f924aef
arthurlch <lachat.arthur74@gmail.com>
2023-03-04 16:57:28 +0900

    chores: add featuresCards, need flexbox implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,16 @@
 import Head from 'next/head';
 import Layout from './_layout';
-import { CodeBlock, dracula } from "react-code-blocks";
+import { CodeBlock, dracula } from 'react-code-blocks';
+import { FiCode } from 'react-icons/fi';
 
-// define props for react-code-blocks 
-
-interface HeroCodeBlock {
+interface CodeBlock {
   language: string;
   showLineNumbers: boolean;
   text: string;
 }
 
-
-function HeroCodeBlock(props: HeroCodeBlock) {
-  const {language, showLineNumbers, text} = props;
+function CodeBlockForHero(props: CodeBlock) {
+  const { language, showLineNumbers, text } = props;
 
   return (
     <CodeBlock
@@ -20,36 +18,81 @@ function HeroCodeBlock(props: HeroCodeBlock) {
       language={language}
       showLineNumbers={showLineNumbers}
       theme={dracula}
-      codeBlock
     />
   );
 }
 
- 
 
-export default function Home() {
+function Hero() {
   const blockLanguage = "html"
   const showLineNumbers = false 
   const blockText = "<h1>Your Snippet here<h1>"
-  
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <CodeBlockForHero 
+          language={blockLanguage}
+          showLineNumbers={showLineNumbers}
+          text={blockText}
+        />
+        <div>
+          <h1 className='text-5xl font-bold'><span className='text-pink-400	'>Simple</span> & <span className='text-pink-400'>Useful</span> snippet manager</h1>              <p className='py-6'>
+          "Unleash your coding potential with our centralized snippet manager - store, share, and access your code snippets securely from anywhere!"
+          </p>
+        </div>
+      </div>
+    </div>
+  ) 
+}
+
+function FeatureCards() {
+  return (
+    <div  className='container mx-auto ml-auto' >
+      <div className='gap-6 columns-1'>
+
+      <div className='card card-compact w-96 bg-base-100 shadow-xl'>
+        <div>
+          <FiCode />
+        </div>
+        <div className='card-body'>
+          <h2 className='card-title'>Any Language</h2>
+          <p>You can simply paste any snippet from any common languages.</p>
+        </div>
+      </div>
+
+      <div className='card card-compact w-96 bg-base-100 shadow-xl'>
+        <div>
+          <FiCode />
+        </div>
+        <div className='card-body'>
+          <h2 className='card-title'>Highlight</h2>
+          <p>Automaticaly highlight your code.</p>
+        </div>
+      </div>
+
+      <div className='card card-compact w-96 bg-base-100 shadow-xl'>
+        <div>
+          <FiCode />
+        </div>
+        <div className='card-body'>
+          <h2 className='card-title'>Easy Search</h2>
+          <p>Search your snippet easily using filter.</p>
+        </div>
+      </div>
+
+      </div>
+    </div>
+    
+  );
+}
+
+export default function Home() {
+
   return (
     <Layout>
-      <main>                                                                                   
-        <div className='hero min-h-screen bg-base-200'>
-          <div className='hero-content flex-col lg:flex-row-reverse'>
-            <HeroCodeBlock 
-              language={blockLanguage}
-              showLineNumbers={showLineNumbers}
-              text={blockText}
-            />
-            <div>
-              <h1 className='text-5xl font-bold'><span className='text-pink-400	'>Simple</span> & <span className='text-pink-400'>Useful</span> snippet manager</h1>              <p className='py-6'>
-              "Unleash your coding potential with our centralized snippet manager - store, share, and access your code snippets securely from anywhere!"
-              </p>
-              <button className='btn btn-primary'>Get Started For Free</button>
-            </div>
-          </div>
-        </div>
+      <main>
+        <Hero/>
+        <FeatureCards />
       </main>
     </Layout>
   );
