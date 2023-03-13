51594a2abe92864d866baf16654b8c1c1450ee79
arthurlch <lachat.arthur74@gmail.com>
2023-03-14 00:42:00 +0900

    chores: cleaning up hero flex positionning

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,32 +13,46 @@ function CodeBlockForHero(props: CodeBlock) {
   const { language, showLineNumbers, text } = props;
 
   return (
-    <CodeBlock
+    <div>
+      <CodeBlock
       text={text}
       language={language}
       showLineNumbers={showLineNumbers}
       theme={dracula}
     />
+    </div>
   );
 }
 
 
 function Hero() {
-  const blockLanguage = "html"
+  const blockLanguage = "javascript"
   const showLineNumbers = false 
-  const blockText = "<h1>Your Snippet here<h1>"
+  const blockText = `function reverseString(str: string): string {
+    let reversedStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i];
+    }
+    return reversedStr;
+  }`;
+  
   return (
-    <div className='hero min-h-screen bg-base-200'>
-      <div className='hero-content flex-col lg:flex-row-reverse'>
-        <CodeBlockForHero 
-          language={blockLanguage}
-          showLineNumbers={showLineNumbers}
-          text={blockText}
-        />
-        <div>
-          <h1 className='text-5xl font-bold'><span className='text-pink-400	'>Simple</span> & <span className='text-pink-400'>Useful</span> snippet manager</h1>              <p className='py-6'>
-          "Unleash your coding potential with our centralized snippet manager - store, share, and access your code snippets securely from anywhere!"
-          </p>
+    <div className='h-screen columns-1 hero  bg-base-200'>
+      <div className='hero-content flex-col flex-wrap lg:flex-row-reverse'>
+        <div className='py-2 my-2'> 
+            <h1 className='text-5xl font-bold'><span className='text-pink-400	'>Simple</span> & <span className='text-pink-400'>Useful</span> snippet manager</h1>              
+            <p className='py-6'>
+            Unleash your coding potential with our centralized snippet manager - store, share, and access your code snippets securely from anywhere!
+            </p>
+        </div>
+        <div className='w-3/6	 flex-col flex '>
+          <div className='items-center place-content-center'>
+            <CodeBlockForHero 
+              language={blockLanguage}
+              showLineNumbers={showLineNumbers}
+              text={blockText}
+            />
+          </div>
         </div>
       </div>
     </div>
