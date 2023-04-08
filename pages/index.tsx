9da4e189616a2aa4ab5b20d7590213d83b5aaae7
arthurlch <lachat.arthur74@gmail.com>
2023-04-09 00:04:38 +0900

    chores: changing theming

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,12 +30,12 @@ function Hero() {
   const blockText = `console.log("rust >> JavaScript")`;
 
   return (
-    <div className="h-screen columns-1 hero  bg-base-200">
+    <div className="h-screen columns-1 hero ">
       <div className="hero-content flex-col flex-wrap lg:flex-row-reverse">
         <div className="py-2 my-2">
           <h1 className="text-5xl font-bold">
-            <span className="text-pink-400	">Simple</span> &{' '}
-            <span className="text-pink-400">Useful</span> snippet manager
+            <span className="text-primary	">Simple</span> &{' '}
+            <span className="text-accent">Useful</span> snippet manager
           </h1>
           <p className="py-6">
             Unleash your coding potential with our centralized snippet manager -
@@ -56,34 +56,34 @@ function Hero() {
 
 function FeatureCards() {
   return (
-    <div className="container mx-auto ml-auto">
+    <div className=" mx-auto ml-auto bg-neutral-focus columns-1">
       <div className=" flex flex-wrap justify-center items-center gap-6 columns-1 py-28 my-23">
-        <div className=" py-4 card card-compact w-96 bg-base-300 shadow-xl 80 md:h-40 lg:h-40">
+        <div className=" py-4 card card-compact w-96 bg-base-100 shadow-xl 80 md:h-40 lg:h-40">
           <div className="flex flex-wrap justify-center items-center">
             <FiCode />
           </div>
           <div className="card-body">
-            <h2 className="card-title text-accent">Any Language</h2>
+            <h2 className="card-title text-secondary">Any Language</h2>
             <p>You can simply paste any snippet from any common languages.</p>
           </div>
         </div>
 
-        <div className=" py-4 card card-compact w-96 bg-base-300 shadow-xl 80 md:h-40 lg:h-40">
+        <div className=" py-4 card card-compact w-96 shadow-xl 80 md:h-40 lg:h-40 bg-base-100">
           <div className="flex flex flex-wrap justify-center items-center ">
             <FiCode />
           </div>
           <div className="card-body">
-            <h2 className="card-title text-primary">Highlight</h2>
+            <h2 className="card-title text-accent">Highlight</h2>
             <p>Automaticaly highlight your code.</p>
           </div>
         </div>
 
-        <div className="py-4 card card-compact w-96 bg-base-300 shadow-xl 80 md:h-40 lg:h-40">
+        <div className="py-4 card card-compact w-96  shadow-xl 80 md:h-40 lg:h-40 bg-base-100">
           <div className="flex flex-wrap justify-center items-center">
             <FiCode />
           </div>
           <div className="card-body">
-            <h2 className="card-title text-secondary">Easy Search</h2>
+            <h2 className="card-title text-primary">Easy Search</h2>
             <p>Search your snippet easily using filter.</p>
           </div>
         </div>
