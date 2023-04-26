d18fb72e9b55cf3492ce7c1269a690190ccd3e13
arthurlch <lachat.arthur74@gmail.com>
2023-04-27 00:24:02 +0900

    fix: mantine navbar type error fixed

--- modules/layout/_layout.tsx
+++ modules/layout/_layout.tsx
@@ -1,56 +1,32 @@
-import { useState } from 'react';
+import { AppShell, useMantineTheme } from '@mantine/core';
 
-import {
-  AppShell,
-  Header,
-  Footer,
-  Text,
-  MediaQuery,
-  Burger,
-  useMantineTheme,
-} from '@mantine/core';
+import Nav from '@/common/components/Nav/Nav';
+import { link } from 'fs';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
-  return (
-    <AppShell
-      styles={{
-        main: {
-          background:
-            theme.colorScheme === 'dark'
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      }}
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      footer={
-        <Footer height={60} p="md">
-          Application footer
-        </Footer>
-      }
-      header={
-        <Header height={{ base: 50, md: 70 }} p="md">
-          <div
-            style={{ display: 'flex', alignItems: 'center', height: '100%' }}
-          >
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
+  const navLinks = [
+    {
+      link: '/',
+      label: 'Home',
+      links: [
+        { link: '/about', label: 'About' },
+        { link: '/contact', label: 'Contact' },
+      ],
+    },
+    {
+      link: '/blog',
+      label: 'Blog',
+      links: [
+        { link: '/posts', label: 'Posts' },
+        { link: '/categories', label: 'Categories' },
+      ],
+    },
+  ];
 
-            <Text>Snippet Hive</Text>
-          </div>
-        </Header>
-      }
-    >
-      <main>{children}</main>
+  return (
+    <AppShell>
+      <Nav links={navLinks} />
+      {children}
     </AppShell>
   );
 }
